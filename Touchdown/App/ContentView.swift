88a0456bad614ee4b895204a2cbd7863d955573a
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-02-05 19:36:39 -0300

    Added BrandGridView and setup

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -37,6 +37,9 @@ struct ContentView: View {
                             }
                             .padding(15)
                             
+                            TitleView(title: "Brands")
+                            BrandGridView()
+                            
                             FooterView()
                                 .padding(.horizontal)
                         }
